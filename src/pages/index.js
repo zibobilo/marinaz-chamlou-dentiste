43160d6c1488630d38c842ae9dbc1d3719bf9c8e
zibobilo <burkhardtraphael@gmail.com>
2021-03-07 12:30:02 -0500

    adding addresses locations

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,14 +32,23 @@ const Home = () => {
         <br />
         <h1>Marinaz Chamlou</h1>
         <h2>Dentisterie esthétique <br />Implantologie</h2>
-        <p>je suis actuellement<br />
-          en déménagement de cabinet.<br />
-          Contactez-moi directement<br />
-          pour prendre rendez-vous.<br />
-          Merci de votre fidélité.<br />
-        </p>
+        <table>
+        <tr>
+          <td>
+            <h3><b>Cabinet dentaire</b></h3>
+            <a href="https://www.google.com/maps/place/Rue+Jean-Baptiste+Meunier+42A,+1050+Ixelles,+Belgium/@50.8147324,4.3489593,17z/data=!3m1!4b1!4m5!3m4!1s0x47c3c457b005f2b5:0x3a36276a70cf2e00!8m2!3d50.8147324!4d4.351148"><p>42A Rue Jean Baptiste Meunier</p>
+            <p>1050 Bruxelles</p></a>
+            <a href="tel:+32 4 77 43 63 50">+32 477 43 63 50</a>
+            </td>
+            <td>
+            <h3><b>Cabinet dentaire <br />CCM Smile</b></h3>
+            <a href="https://www.google.com/maps/place/Avenue+de+Tervueren+266,+1150+Woluwe-Saint-Pierre,+Belgium/@50.8355955,4.4162118,17z/data=!3m1!4b1!4m5!3m4!1s0x47c3db501ac7e2db:0x34fdabd00009488c!8m2!3d50.8355955!4d4.4184058"><p>Avenue de Tervueren 266</p>
+            <p>1150 Woluwe-saint-Pierre</p></a>
+            <a href="tel:+32 2 732 48 29">+32 2 732 48 29</a>
+          </td>
+        </tr>
+        </table>
         <a href="mailto:chamlou.dentaire@gmail.com?subject=Rendez-vous via dentistebxl.com">chamlou.dentaire@gmail.com</a><br /><br />
-        <a href="tel:+32 477 43 63 50">+32 477 43 63 50</a><br />
         </div>
       </div>
     </main>
